3793a858cd74c5371af40ca46f2e7b0d0a217e79
Saad Makrod <saad.makrod@gmail.com>
2021-08-03 11:01:53 -0400

    Deleting unnecessary code - Saad

--- app/src/main/java/com/example/medicalclinic/PatientUser.java
+++ app/src/main/java/com/example/medicalclinic/PatientUser.java
@@ -7,8 +7,6 @@ import java.util.Date;
 public abstract class PatientUser extends User {
     String name, password, gender, username;
     String date_of_birth;                //Can be either date like Java.Date() or string
-    //AppointmentListPro upcoming_appointments; //List of dates like Java.Date()
-    //AppointmentList previous_appointments; //List of dates like Java.Date()
     List<AppointmentInfo> doctors;                //List of doctor id and name no need to have other info
 
     public abstract void book(String doc_username, Date d, String patient_username) throws ParseException; //Book appointments
@@ -19,17 +17,8 @@ public abstract class PatientUser extends User {
 
     public abstract void setDate_of_birth(String date_of_birth);
 
-    //public abstract AppointmentListPro getUpcoming_appointments();
-
-    //public abstract void setUpcoming_appointments(AppointmentListPro upcoming_appointments);
-
-    //public abstract AppointmentList getPrevious_appointments();
-
-    //public abstract void setPrevious_appointments(AppointmentList previous_appointments);
-
     public abstract List<AppointmentInfo> getDoctors();
 
     public abstract void setDoctors(List<AppointmentInfo> doctors);
-
 }
 
